d64d8df5a1bda1c27e2a23fc61d506ccc081511f
Vladimír Kroupa <vladimirkroupa@gmail.com>
2022-02-19 08:09:02 +0100

    Use Riseup account/smtp instead of Gmail

--- mailer.py
+++ mailer.py
@@ -4,14 +4,14 @@ from email.message import Message
 from typing import List
 from email.mime.text import MIMEText
 
-FROM = "bikekitchenpraha@gmail.com"
+FROM = "bikekitchenpraha@riseup.net"
 
 
 def send_email(recipients: List[str], subject: str, msg_body: str, password: str):
     msg = MIMEText(msg_body)
 
     msg['Subject'] = subject
-    msg['From'] = "bikekitchenpraha@gmail.com"
+    msg['From'] = FROM
     msg['To'] = ','.join(recipients)
 
     _send_email(msg, password)
@@ -20,7 +20,7 @@ def send_email(recipients: List[str], subject: str, msg_body: str, password: str
 def _send_email(message: Message, password: str):
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as server:
-        server.login("bikekitchenpraha@gmail.com", password)
+    with smtplib.SMTP_SSL("mail.riseup.net", port=465, context=context) as server:
+        server.login("bikekitchenpraha@riseup.net", password)
         server.send_message(message)
         server.quit()
