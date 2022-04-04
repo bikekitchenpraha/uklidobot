f23f5fe0b15a269cbb7e568f25e37c68c0802937
Vladimír Kroupa <vladimirkroupa@gmail.com>
2022-04-04 18:44:48 +0200

    Fix current/next group, add next group to email template

--- sheets.py
+++ sheets.py
@@ -4,18 +4,17 @@ from typing import Tuple, List
 
 from gspread import Worksheet, Client
 
-# SUN
-NOTIFY_ON_DOW = 0
+from model import Group
 
 
-def determine_groups(worksheet: Worksheet) -> List[Tuple[int, str]]:
+def determine_groups(worksheet: Worksheet) -> Tuple[Group, Group, Group]:
     def find_current_week_row(dates, current_year, current_week):
         for ix, date in enumerate(dates):
             year, week, _ = date.isocalendar()
             if year == current_year and week == current_week:
                 return ix + 2
 
-    def to_group_tuple(row) -> Tuple[int, str]:
+    def to_group_tuple(row) -> Group:
         return int(row[1]), str(row[2])
 
     dates_col = worksheet.col_values(1)
@@ -24,9 +23,10 @@ def determine_groups(worksheet: Worksheet) -> List[Tuple[int, str]]:
 
     year, week, _ = datetime.datetime.today().isocalendar()
     row = find_current_week_row(dates, year, week)
+    prev_wk_group = to_group_tuple(worksheet.row_values(row - 1))
     current_wk_group = to_group_tuple(worksheet.row_values(row))
     next_wk_group = to_group_tuple(worksheet.row_values(row + 1))
-    return [current_wk_group, next_wk_group]
+    return prev_wk_group, current_wk_group, next_wk_group
 
 
 def find_group_members(worksheet: Worksheet, group_no: int, group_name: str) -> List[str]:
@@ -53,15 +53,14 @@ def find_member_addresses(people_ws: Worksheet, members: List[str]) -> List[Tupl
     return [(person, email) for person, email in zip(people, emails) if person in members]
 
 
-def determine_current_group_and_next(client) -> Tuple[Tuple[int, str]]:
+def determine_prev_curr_next_groups(client) -> Tuple[Group, Group, Group]:
     cal_sheet = client.open_by_key('1tbtHGLT0jvMGMOUF7laOwIOX65szHTkAuFSfGQlwM0I')
     roster_ws = cal_sheet.worksheet('Úklid chodby')
 
-    curr_group, next_group = determine_groups(roster_ws)
-    return curr_group, next_group
+    return determine_groups(roster_ws)
 
 
-def determine_group_members(client, group: Tuple[int, str]) -> List[Tuple[str, str]]:
+def determine_group_members(client, group: Group) -> List[Tuple[str, str]]:
     group_no, group_name = group
 
     groups_sheet = client.open_by_key('1E9TVhHrWORau9sISgL-9yoCsRdnklr3LYwTPz-FMJqQ')
